3a22c5369ed47acf224b20b0d8cbf0cd28235830
Thomas Hart <t.j.hart89@hotmail.co.uk>
2022-12-19 16:52:12 +0000

    Start homepage and card

--- frontend_fourloop/src/App.js
+++ frontend_fourloop/src/App.js
@@ -4,17 +4,21 @@ import Home from "./components/Home";
 import React from "react";
 import Profile from "./components/Profile";
 import Navbar from "./components/Navbar";
+import "./App.css";
+import logo from "./assets/coders-logo.png";
+import Card from "./components/Card";
 
 function App() {
   return (
     <div>
-      <p>hi</p>
+      <img src={logo} alt="coders-logo" />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/create" element={<Create />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
+      <Card />
     </div>
   );
 }
