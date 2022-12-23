954d356058423be63532f58f56ab4a09f073c904
Junior Gabi <juniorgabi@juniors-air.home>
2022-12-23 14:08:49 +0000

    create page finished done

--- frontend_fourloop/src/components/Create.jsx
+++ frontend_fourloop/src/components/Create.jsx
@@ -13,8 +13,10 @@ export default function Create() {
   return (
     <div className="create-post">
       <form className="ui form">
+      <div className="form-field">
       <div className="field">
       <label>Select a file</label>
+      <h2>Select a file:</h2>
         <input
           type="file"
           name="Myfile"
@@ -26,10 +28,11 @@ export default function Create() {
           <img id="image" src={URL.createObjectURL(uploadImg)} />
         ) : null}
       <div class="field">
-    
+      <h2>Caption:</h2>
   <textarea rows="2" cols="50" id="text"></textarea>
   </div>
       <button className="ui button" type="submit" onClick={(e) => {e.preventDefault()}}>Submit</button>
+      </div>
       </div>
       </form>
     </div>
